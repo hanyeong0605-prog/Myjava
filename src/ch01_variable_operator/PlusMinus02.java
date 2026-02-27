1bf2a484f1c8bc574b1786e77899ce32394c721f
hanyeong10 <hanyeong10@gmail.com>
2026-02-27 17:37:07 +0900

    증감 연산자 예제 추가

---
+++ src/ch01_variable_operator/PlusMinus02.java
@@ -0,0 +1,21 @@
+package ch01_variable_operator;
+
+public class PlusMinus02 {
+    static void main() {
+
+        int x = 3, y = 5, z;
+
+        z = x++ + --y;
+
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+        System.out.println("z: " + z);
+
+
+        z += --x + y++;
+
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+        System.out.println("z: " + z);
+    }
+}
