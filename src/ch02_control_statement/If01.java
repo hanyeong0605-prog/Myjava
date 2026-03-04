2f2070508e9ed3e0b67a2f82dc0b7e918435fcef
hanyeong65 <hanyeong0605@gmail.com>
2026-03-04 10:27:34 +0900

    하하하 호호호

---
+++ src/ch02_control_statement/If01.java
@@ -0,0 +1,15 @@
+package ch02_control_statement;
+
+public class If01 {
+    static void main() {
+        //su가 짝수이면 출력해 주세요.
+        int su = 5 ;
+        if(su % 2 == 0) {
+            System.out.println(su+"는 짝수 ");
+        }
+        else {
+            System.out.println(su+"는 홀수  ");
+        }
+        System.out.println("나는 무조건 실행됩니다. ");
+    }
+}
---
+++ src/ch02_control_statement/If02.java
@@ -0,0 +1,14 @@
+package ch02_control_statement;
+
+public class If02 {
+    static void main() {
+        int su = 4;
+        if (su % 2 == 0) { //짝수이면
+            System.out.println("하하하");
+        }
+
+        else { //홀수이면
+            System.out.println("호호호 ");
+        }
+    }
+}
