af20cf970bd7e9f9932a4722ec2e4153c80a30a4
hanyeong0605-prog <hanyeong0605@gmail.com>
2026-03-04 10:58:24 +0900

    하하하 호호호

--- src/ch02_control_statement/If02.java
+++ src/ch02_control_statement/If02.java
@@ -5,10 +5,12 @@ public class If02 {
         int su = 4;
         if (su % 2 == 0) { //짝수이면
             System.out.println("하하하");
+            System.out.println(su + "는 짝수");
         }
 
         else { //홀수이면 호호호 히하히하히히히
             System.out.println("호호호 ");
+            System.out.println(su + "는 홀수 ");
         }
     }
 }
