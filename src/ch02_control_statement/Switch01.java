095949563c3c66882b9398c266e7fecc2942b106
hanyeong0605-prog <hanyeong0605@gmail.com>
2026-03-04 11:38:05 +0900

    하하하 호호호

--- src/ch02_control_statement/Switch01.java
+++ src/ch02_control_statement/Switch01.java
@@ -3,7 +3,7 @@ package ch02_control_statement;
 public class Switch01 {
     static void main() {
         // 주사위 눈금의 수가 짝수인지 홀수 인지 판단하기
-        int su = 5;
+        int su = 5  ;
 
 
         switch (su){
@@ -25,8 +25,6 @@ public class Switch01 {
             case 6 :
                 System.out.println("짝수");
                 break;
-
-
         }
     }
 }
